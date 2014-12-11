f538e01e6736e09739b96373a80e61e261e51b2c
BoppreH <boppreh@gmail.com>
2014-12-11 01:25:14 -0200

    Add indexing and repr

--- matrix.py
+++ matrix.py
@@ -1,14 +1,36 @@
 class Matrix(object):
-    def __init__(self, nrows, ncols=None):
-        if ncols is None:
+    def __init__(self, nrows=None, ncols=None):
+        if nrows is None:
+            self.nrows = 3
+            self.ncols = 3
+            self.m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        elif ncols is None:
             matrix = nrows
-            self.m = matrix
             self.nrows = len(matrix)
             self.ncols = len(matrix[0])
+            self.m = matrix
         else:
             self.nrows = nrows
             self.ncols = ncols
             self.m = [[None] * ncols for row in range(nrows)]
 
+        self.height = nrows
+        self.width = ncols
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            return self.m[index]
+        elif isinstance(index, tuple):
+            row, col = index
+            return self.m[row][col]
+        else:
+            raise TypeEror("Invalid index type " + str(index))
+
+    def __repr__(self):
+        return '\n'.join(' '.join(map(str, line)) for line in self.m)
+
     def __eq__(self, other):
-        return isinstance(other, Matrix) and self.m == other.m
+        if isinstance(other, Matrix):
+            return self.m == other.m
+        else:
+            return self.m == other
--- tests.py
+++ tests.py
@@ -6,6 +6,20 @@ class Test(unittest.TestCase):
         m1 = Matrix(2, 3)
         m2 = Matrix([[None, None, None], [None, None, None]])
         self.assertEqual(m1, m2)
+        self.assertEqual(3, m1.width)
+        self.assertEqual(2, m1.height)
+        self.assertEqual([[1, 2, 3], [4, 5, 6], [7, 8, 9]], Matrix())
+
+    def test_index(self):
+        m1 = Matrix()
+
+        self.assertEqual(1, m1[0,0])
+        self.assertEqual(2, m1[0,1])
+        self.assertEqual(4, m1[1,0])
+        self.assertEqual(9, m1[2,2])
+
+        self.assertEqual([1, 2, 3], m1[0])
+        self.assertEqual([7, 8, 9], m1[2])
 
 if __name__ == '__main__':
     unittest.main()
