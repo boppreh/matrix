169a2659f87df9b3fd59e0de7591e304d80a52f2
BoppreH <boppreh@gmail.com>
2015-08-26 18:34:50 -0300

    Add comments to Tic Tac Toe

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,6 @@
+"""
+Generalized Tic Tac Toe game for any board size, as example for Matrix usage.
+"""
 from matrix import Matrix
 from itertools import groupby
 
@@ -6,16 +9,24 @@ win_size = int(input('Sequence size to win (default 3): ') or 3)
 
 # Create NxN matrix filled with numbers from 1 to N^2+1.
 count = iter(range(1, size * size + 1))
+
+# The Matrix method `map` calls fn(cell_value) for every cell and builds a new
+# matrix with the results. Here we fill the matrix with numbers from 1 to
+# N^2+1.
 board = Matrix(size, size).map(lambda i: next(count))
 
 turn = 'x'
 
 def check_winner():
+    """ Returns the winner symbol or None. """
+    # The attributes `rows`, `cols` and `diagonals` list all items taken by
+    # rows, columns and diagonals (all diagonals in both directions),
+    # respectively.
     for line in board.rows + board.cols + board.diagonals:
         # ['x', 'x', 'o'] -> [('x', 2), ('o', 1)]
-        for stone, sequence in groupby(line):
+        for symbol, sequence in groupby(line):
             if len(list(sequence)) == win_size:
-                return stone
+                return symbol
 
 while True:
     print('\n{} turn:\n{}'.format(turn, board))
@@ -23,8 +34,9 @@ while True:
     while True:
         try:
             play = int(input(turn + ': '))
-            assert board[play - 1] not in ('x', 'o')
-            board[play - 1] = turn
+            # The method `index` returns (row, col) for the first occurrence of
+            # the given value.
+            board[board.index(play)] = turn
             break
         except Exception:
             pass
