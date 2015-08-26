e1397b4ab78cac7fac5bd98e25606e6d344fdf84
BoppreH <boppreh@gmail.com>
2015-08-26 19:17:57 -0300

    Refactor tic tac toe

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,51 +2,59 @@
 Generalized Tic Tac Toe game for any board size, as example for Matrix usage.
 """
 from matrix import Matrix
-from itertools import groupby
+from collections import defaultdict
 
 size = int(input('Board size (default 3): ') or 3)
 win_size = int(input('Sequence size to win (default 3): ') or 3)
 
-# Create NxN matrix filled with numbers from 1 to N^2+1.
 count = iter(range(1, size * size + 1))
-
 # The Matrix method `map` calls fn(cell_value) for every cell and builds a new
 # matrix with the results. Here we fill the matrix with numbers from 1 to
 # N^2+1.
-board = Matrix(size, size).map(lambda i: next(count))
+board = Matrix(size, size, '').map(lambda i: str(next(count)))
 
 turn = 'x'
 
-def check_winner():
-    """ Returns the winner symbol or None. """
-    # The attributes `rows`, `cols` and `diagonals` list all items taken by
-    # rows, columns and diagonals (all diagonals in both directions),
-    # respectively.
-    for line in board.rows + board.cols + board.diagonals:
-        # ['x', 'x', 'o'] -> [('x', 2), ('o', 1)]
-        for symbol, sequence in groupby(line):
-            if len(list(sequence)) == win_size:
-                return symbol
-
 while True:
+    # When converted to string, the board prints itself as a NxM matrix with
+    # aligned columns, such as this:
+    #
+    #  1  2  3  4
+    #  5  6  7  8
+    #  9 10 11 12
     print('\n{} turn:\n{}'.format(turn, board))
 
     while True:
         try:
-            play = int(input(turn + ': '))
-            # The method `index` returns (row, col) for the first occurrence of
-            # the given value.
-            board[board.index(play)] = turn
+            play = input(turn + ': ')
+            # Player entered a number, now search for this number in our board.
+            row, col = board.index(play)
+            assert board[row, col] not in ('x', 'o')
+            board[row, col] = turn
             break
-        except Exception:
+        except Exception as e:
+            print(e)
             pass
 
-    if check_winner():
-        clean_board = board.map(lambda i: i if i in ('x', 'o') else ' ')
-        print('\n{} wins!\n{}'.format(turn, clean_board))
-        break
-    elif all(i in ('x', 'o') for i in board):
+    # Make a list of all lines that pass through the cell played.
+    directions = [board.row(row),
+                  board.col(col),
+                  board.diagonal(row, col, +1),
+                  board.diagonal(row, col, -1)]
+
+    # Convert lines to a single space separated string.
+    str_directions = ' '.join(''.join(line) for line in directions)
+
+    # Verify if there are three of the player symbols in a row.
+    if turn * 3 in str_directions:
+        # "Translate" the board by passing the cells through this translation
+        # dictionary. This filters out the numbers and replaces them with dots.
+        translation = defaultdict({'x': 'x', 'o': 'o'}, '.')
+        print('\n{} wins!\n{}'.format(turn, board.translate(translation)))
+        exit()
+
+    if all(i in ('x', 'o') for i in board):
         print('\nDraw!\n{}'.format(board))
         break
 
-    turn = {'x': 'o', 'o': 'x'}[turn]
+    turn = 'x' if turn == 'x' else 'o'
